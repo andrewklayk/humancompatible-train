5feebedfbc287c9b26815a5adf7be035dbeaba45
andre <andrew.klyachkin@gmail.com>
2025-08-18 19:34:50 +0200

    rework structure

--- src/algorithms/__init__.py
+++
@@ -1,6 +0,0 @@
-from .ghost import StochasticGhost
-from .ssl_alm import SSLALM
-from .switching_subgradient import SSG
-from .sgd import SGD
-
-__all__ = ["SSLALM", "StochasticGhost", "SSG", "SGD"]
