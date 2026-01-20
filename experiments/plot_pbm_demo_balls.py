35d15df67dfc13d90e83a1ce3c55eaf008fb4158
bosakad <bosadam@seznam.cz>
2026-01-20 17:26:42 +0100

    pbm demo reg. update

--- experiments/plot_pbm_demo_balls.py
+++ experiments/plot_pbm_demo_balls.py
@@ -1,20 +1,22 @@
 from matplotlib.patches import Circle
 import torch
 from humancompatible.train.optim.PBM import PBM
+import numpy as np
+import matplotlib.pyplot as plt
 
-def plot_balls_trajectory(trajectory):
+def plot_balls_trajectory(trajectory_pbm, trajectory_sgd):
     """
     trajectory: array-like of shape (N, 2), where each row is [x, y]
     """
 
-    trajectory = np.asarray(trajectory)
+    trajectory_pbm = np.asarray(trajectory_pbm)
 
-    fig, ax = plt.subplots(figsize=(6, 4))
+    fig, ax = plt.subplots(figsize=(12, 8))
 
     # Feasible regions: unit balls
     ball_centers = [(-2, 0), (2, 0)]
-    radius = 1.0
-    labels = [r"$g_1(x,y) \leq 0 $", r"$g_2(x,y) \leq 0$"]
+    radius = np.sqrt(0.99)
+    labels = [r"$\mathbb{E}[g_1(x,y,\xi)] \leq 0 $", r"$\mathbb{E}[g_2(x,y,\xi)] \leq 0$"]
 
     for center, label in zip(ball_centers, labels):
         ball = Circle(
@@ -29,9 +31,9 @@ def plot_balls_trajectory(trajectory):
         ax.add_patch(ball)
         # Label inside the ball
         ax.text(
-            center[0], center[1] - 0.5,
+            center[0], center[1],
             label,
-            fontsize=10,
+            fontsize=12,
             fontweight='bold',
             color='black',
             ha='center',
@@ -41,16 +43,25 @@ def plot_balls_trajectory(trajectory):
 
     # Trajectory
     ax.plot(
-        trajectory[:, 0],
-        trajectory[:, 1],
+        trajectory_sgd[:, 0],
+        trajectory_sgd[:, 1],
+        color="red",
+        linewidth=2.0,
+        zorder=3,
+    )
+
+    # Trajectory
+    ax.plot(
+        trajectory_pbm[:, 0],
+        trajectory_pbm[:, 1],
         color="black",
         linewidth=2.0,
         zorder=3,
     )
 
     # Emphasize x_0 and x_n
-    x0 = trajectory[0]
-    xn = trajectory[-1]
+    x0 = trajectory_pbm[0]
+    xn = trajectory_pbm[-1]
 
     ax.scatter(
         x0[0], x0[1],
@@ -80,7 +91,28 @@ def plot_balls_trajectory(trajectory):
         zorder=5,
     )
     ax.annotate(
-        r"$x_n$",
+        r"$x^\text{spbm}_n$",
+        xy=(xn[0], xn[1]),
+        xytext=(6, 8),
+        textcoords="offset points",
+        fontsize=12,
+        zorder=5,
+    )
+
+        # Emphasize x_0 and x_n
+    xn = trajectory_sgd[-1]
+
+    ax.scatter(
+        xn[0], xn[1],
+        s=80,
+        marker="s",
+        facecolor="red",
+        edgecolor="red",
+        zorder=4,
+    )
+
+    ax.annotate(
+        r"$x^{\text{reg}}_n$",
         xy=(xn[0], xn[1]),
         xytext=(6, 8),
         textcoords="offset points",
@@ -107,6 +139,7 @@ def plot_balls_trajectory(trajectory):
     ax.set_ylim(-2.5, 2.5)
 
     plt.tight_layout()
+    plt.savefig("demo_balls_pbm.pdf")
     plt.show()
 
 
@@ -123,22 +156,65 @@ def balls(x, sample):
 def parabola(x):
     return x[0]**2 + x[1]**2
 
+
+samples = [
+    # torch.tensor([0]),
+    # torch.tensor([0])
+    torch.tensor([-0.1]),
+    torch.tensor([0.1])
+]
+
+
+################## SGD ######################### 
+
 xy = torch.nn.Parameter(data=torch.ones(2, requires_grad=True))
 with torch.no_grad():
     xy[0] = 0
     xy[1] = 1
 
-pbm = PBM([xy], m=1, lr=0.01, dual_bounds=(1e-3, 1e3), penalty_update_m='CONST', epoch_len=2, mu=0, opt_method="SGD")
+sgd = torch.optim.SGD([xy], lr=0.02, dampening=0.1)
 
-samples = [
-    # torch.tensor([0]),
-    # torch.tensor([0])
-    torch.tensor([-0.2]),
-    torch.tensor([0.2])
-]
-
+scheduler = torch.optim.lr_scheduler.LambdaLR(
+    sgd,
+    lr_lambda=lambda step: 0.99 ** step
+)
 
 iters = 100
+
+param_log_sgd = []
+con_log_sgd = []
+dual_log_sgd = []
+c_grad_log_sgd = []
+
+for i in range(iters):
+    param_log_sgd.append(
+        xy.detach().numpy().copy()
+    )
+    
+    minibatch = samples[i % 2]
+    
+    c = balls(xy, minibatch)
+
+    obj = parabola(xy) + torch.norm(c)
+
+    obj.backward()
+    sgd.step()
+    scheduler.step()
+    sgd.zero_grad()
+
+    con_log_sgd.append(c.detach().numpy().copy().item())
+
+
+
+################## PBM ######################### 
+
+xy = torch.nn.Parameter(data=torch.ones(2, requires_grad=True))
+with torch.no_grad():
+    xy[0] = 0
+    xy[1] = 1
+
+pbm = PBM([xy], m=1, lr=0.008, dual_bounds=(1e-3, 1e3), penalty_update_m='CONST', epoch_len=2, mu=0, opt_method="SGD")
+
 
 param_log = []
 con_log = []
@@ -162,7 +238,7 @@ for i in range(iters):
 
     pbm.step(obj)
     for gr in pbm.param_groups:
-        gr['lr'] *= 0.95
+        gr['lr'] *= 0.98
 
     con_log.append(c.detach().numpy().copy().item())
 
@@ -170,15 +246,11 @@ for i in range(iters):
 # print(param_log)
 # print(con_log)
 
-from matplotlib import pyplot as plt
-import numpy as np
-
-
 plot_balls_trajectory(
-    np.array(param_log)
+    np.array(param_log), np.array(param_log_sgd)
 )
 
-print(param_log)
+# print(param_log)
 # print(np.array(dual_log))
 # print(np.array(con_log))
-print(np.array(con_log))
+# print(np.array(con_log))
