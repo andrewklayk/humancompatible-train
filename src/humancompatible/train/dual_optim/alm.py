4eff1a64cd27af1ac8259ce6ad176506eedda62f
andrewklayk <andrew.klyachkin@gmail.com>
2026-02-05 15:35:26 +0100

    new api for alm, moreau

---
+++ src/humancompatible/train/dual_optim/alm.py
@@ -0,0 +1,124 @@
+import torch
+from torch.nn import Parameter
+from torch.optim import Optimizer
+from typing import Any, Iterable, Tuple
+from torch import clamp_, Tensor, no_grad
+from torch.optim.optimizer import _use_grad_for_differentiable
+
+
+class ALM(Optimizer):
+    def __init__(
+        self,
+        m: int = None,
+        lr: float = 0.01,  # | Iterable[float] = 0.01 , TODO: individual LRs for each constraint
+        init_duals: float | Tensor = None,
+        penalty: float = 1.0,
+        *,
+        dual_range: Tuple[float, float] = (0.0, 100.0),
+    ) -> None:
+        """
+        A wrapper over a PyTorch`Optimizer` that works on the dual maximization tasks according to the Augmented Lagrangian rule. Creates and updates dual variables.
+
+        :param m: Number of constraints (determines the number of dual variables to create)
+        :type m: int
+        :param lr: Dual variable update rate 
+        :type lr: float
+        :param init_duals: Initial values for the new dual variables. Defaults to 0 for all.
+        :type init_duals: float | Tensor
+        :param penalty: Augmented Lagrangian penalty parameter. Defaults to`1.`
+        :type penalty: float
+        :param dual_range: Safeguarding range for dual variables; they will be`clamp`-ed to this range.
+        :type dual_range: Tuple[float, float]
+        """
+
+        ## checks ##
+        if m is None and not isinstance(init_duals, Tensor):
+            raise ValueError("At least one of`m`,`init_duals` must be set")
+        m = m if m is not None else len(init_duals)
+        if isinstance(lr, Iterable) and len(lr) != m:
+            raise ValueError("`lr`should be the same length as`init_duals`or`m`")
+
+        self.penalty = penalty
+        self.dual_range = dual_range
+
+        defaults = {"lr": lr}
+        self.defaults = defaults
+
+        if init_duals is None:
+            init_duals = torch.zeros(m, requires_grad=False)
+
+        duals = Parameter(init_duals, requires_grad=False)
+        super().__init__([duals], defaults)
+
+
+    @property
+    def duals(self) -> Tensor:
+        """
+        :return: Dual variables, concatenated into a single tensor.
+        :rtype: Tensor
+        """
+        return torch.cat([group["params"][0] for group in self.param_groups])
+
+    def add_constraint_group(
+        self, m: int = None, lr: float = None, init_duals: Tensor = None
+    ) -> None:
+        """
+        Allows to add a group of dual variables with separate initial values and learning rates.
+
+        :param m: Size of group (number of dual variables to add)
+        :type m: int
+        :param lr: Dual variable update rate
+        :type lr: float
+        :param init_duals: Initial values for the new dual variables
+        :type init_duals: Tensor
+        """
+        if init_duals is None and m is None:
+            raise ValueError("At least one of`size`,`init_duals` must be set")
+        if isinstance(lr, Iterable) and len(lr) != m:
+            raise ValueError("`lr`should be the same length as`init_duals`or`m`")
+        if init_duals is None:
+            init_duals = Parameter(torch.zeros(m))
+        param_group_dict = {"params": [init_duals]}
+        if lr is not None:
+            param_group_dict["lr"] = lr
+        self.add_param_group(param_group_dict)
+
+
+    # evaluate the Lagrangian and update the dual variables
+    def forward_update(self, loss: Tensor, constraints: Tensor) -> Tensor:
+        lagrangian = torch.zeros_like(loss)
+        lagrangian.add_(loss)
+        for i, group in enumerate(self.param_groups):
+            duals, lr = group["params"][0], group["lr"]
+            group_constraints = constraints[i * len(duals) : (i + 1) * len(duals)]
+            with torch.no_grad():
+                _update_duals(duals, group_constraints, lr)
+                clamp_(duals, min=self.dual_range[0], max=self.dual_range[1])
+
+            lagrangian.add_(duals @ group_constraints)
+
+        if self.penalty > 0:
+            lagrangian.add_(
+                0.5 * self.penalty * torch.square(torch.linalg.norm(constraints, ord=2))
+            )
+
+        return lagrangian
+
+    def state_dict(self) -> dict[str, Any]:
+
+        packed_state = {"penalty": self.penalty, "dual_range": self.dual_range}
+        state_dict = {"state": packed_state, "param_groups": self.param_groups}
+
+        return state_dict
+
+    def load_state_dict(self, state_dict: dict[str, Any]) -> None:
+        self.penalty = state_dict["state"]["penalty"]
+        self.dual_range = state_dict["state"]["dual_range"]
+        params = state_dict["param_groups"]
+        self.param_groups = []
+        for param in params:
+            self.param_groups.append(param)
+
+
+def _update_duals(duals: Tensor, constraints: Tensor, lr: float) -> None:
+    duals.add_(constraints, alpha=lr)
---
+++ src/humancompatible/train/dual_optim/moreau.py
@@ -0,0 +1,84 @@
+import torch
+from torch.optim import Optimizer
+from torch.optim.optimizer import _use_grad_for_differentiable
+from torch import no_grad
+
+
+class MoreauEnvelope(Optimizer):
+    def __init__(
+        self,
+        optimizer: torch.optim.Optimizer,
+        *,
+        mu: float = 2.,
+        beta: float = 0.5,
+    ) -> None:
+        """
+        A wrapper over a PyTorch`Optimizer` that allows for quick calculation of the Moreau envelope.
+
+        :param optimizer: Optimizer.
+        :type torch.optim.Optimizer: int
+        :param mu: Smoothing multiplier; defaults to 0.
+        :type mu: float
+        :param beta: Smoothing multiplier update rate; defaults to 0.5.
+        :type beta: float
+        """
+
+        self.optimizer = optimizer
+        self.mu, self.beta = mu, beta
+
+        if mu <= 0:
+            raise ValueError(f"The smoothing parameter`mu`should be positive, got {mu}.")
+        else:
+            self.smoothing_buffer = []
+            for param_group in optimizer.param_groups:
+                self.smoothing_buffer.append({'params': []})
+                for _, param in enumerate(param_group['params']):
+                    self.smoothing_buffer[-1]['params'].append(param.clone().detach())
+
+    def step(self) -> None:
+        with torch.no_grad():
+            # add smoothing term gradient to the gradient w.r.t. primal params, and update smoothing params before optimizer step
+            for param_group, smoothing_buffer_group in zip(self.optimizer.param_groups, self.param_groups):
+                for param, smoothing_buffer in zip(param_group["params"], smoothing_buffer_group['params']):
+                    param.grad.add_(param - smoothing_buffer, alpha=self.mu)
+                    smoothing_buffer.add_(param).add_(smoothing_buffer, alpha=-self.beta)# - smoothing_buffer, alpha=self.beta)
+        
+        self.optimizer.step()
+
+
+    def __getattr__(self, name):
+        # Delegate to the wrapped object
+        try:
+            attr = getattr(self.optimizer, name)
+        except AttributeError:
+            raise AttributeError(f"'A' object has no attribute '{name}'")
+        else:
+            # If it's a method, bind it to self.optimizer
+            if callable(attr):
+                def method(*args, **kwargs):
+                    return attr(self.optimizer, *args, **kwargs)
+                return method
+            else:
+                return attr
+
+    # TODO: do we need to save the smoothing buffer?
+
+    # def state_dict(self) -> dict[str, Any]:
+
+    #     dual_packed_state = {"smoothing_buffer": self.smoothing_buffer, "mu": self.mu, "beta": self.beta}
+    #     dual_state_dict = {"state": dual_packed_state, "param_groups": self.param_groups}
+    #     state_dict = {"primal": self.optimizer.state_dict(), "dual": dual_state_dict}
+    #     return state_dict
+
+    # def load_state_dict(self, state_dict: dict[str, Any]) -> None:
+    #     primal_state_dict = state_dict["primal"]
+    #     self.primal_optimizer.load_state_dict(primal_state_dict)
+        
+    #     dual_state_dict = state_dict["dual"]
+    #     self.penalty = dual_state_dict["state"]["penalty"]
+    #     self.dual_range = dual_state_dict["state"]["dual_range"]
+    #     self.smoothing_buffer = state_dict["state"]["smoothing_buffer"]
+    #     params = dual_state_dict["param_groups"]
+    #     self.param_groups = []
+    #     for param in params:
+    #         self.param_groups.append(param)
