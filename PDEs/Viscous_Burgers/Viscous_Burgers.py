7a744c6848d96444f7eaa3f3dfafb8e62ac1ceab
bosakad1 <bosakad1@login3.rci.cvut.cz>
2026-05-05 10:56:02 +0200

    viscous burgers PDE implemented

--- PDEs/Viscous_Burgers/Viscous_Burgers.py
+++ PDEs/Viscous_Burgers/Viscous_Burgers.py
@@ -2,8 +2,10 @@ from tqdm import tqdm
 import pickle as pkl
 import numpy as np
 import copy
-import sys
 import argparse
+import sys
+sys.path.append("..")
+
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
@@ -15,34 +17,23 @@ from humancompatible.train.dual_optim import ALM, MoreauEnvelope, PBM
 from networks import set_model, u_Net_shallow_wide, u_Net_shallow_wide_resnet, u_Net_deep_narrow, u_Net_deep_narrow_resnet
 
 # Equation parameter
-k, a1, a2 = 1, 1, 4
 
-def q(data) :
-    x, y = data[:,0].view(-1,1), data[:,1].view(-1,1)
-    return -((a1*np.pi)**2)*torch.sin(a1*np.pi*x)*torch.sin(a2*np.pi*y) \
-           -((a2*np.pi)**2)*torch.sin(a1*np.pi*x)*torch.sin(a2*np.pi*y) \
-           +(k**2)*torch.sin(a1*np.pi*x)*torch.sin(a2*np.pi*y)
+nu = 0.01/np.pi
 
-def analytic(data) :
-    x, y = data[:,0].view(-1,1), data[:,1].view(-1,1)
-    return torch.sin(a1*np.pi*x)*torch.sin(a2*np.pi*y)
-    
 def calculate_derivative(y, x) :
     return torch.autograd.grad(y, x, create_graph=True,\
                         grad_outputs=torch.ones(y.size()).to(device))[0]
 
-
 def calculate_all_partial(u, x) :
     del_u = calculate_derivative(u, x)
-    u_x, u_y = del_u[:,0], del_u[:,1]
-    u_xx = calculate_derivative(u_x, x)[:,0]
-    u_yy = calculate_derivative(u_y, x)[:,1]
-    return u_xx.view(-1,1), u_yy.view(-1,1)
+    u_t, u_x = del_u[:,0], del_u[:,1]
+    u_xx = calculate_derivative(u_x, x)[:,1]
+    return u_t.view(-1,1), u_x.view(-1,1), u_xx.view(-1,1)
 
 
-def train(u_model, beta, trainloader, bdry_data, val_test, optimizer, loss_f, dual_opt=None) :
-    loss_list, loss_list1, loss_list2, val_list, test_list = [], [], [], [], []
-    X_bdry, u_bdry = bdry_data
+def train(u_model, beta, trainloader, ini_bdry_data, val_test, optimizer, loss_f, dual_opt=None) :
+    loss_list, loss_list1, loss_list2, loss_list3, val_list, test_list = [], [], [], [], [], []
+    X_ini, u_ini, X_bdry, u_bdry = ini_bdry_data
     X_val, y_val, X_test, y_test = val_test
 
     for i, (data,) in enumerate(trainloader) :
@@ -50,56 +41,65 @@ def train(u_model, beta, trainloader, bdry_data, val_test, optimizer, loss_f, du
         optimizer.zero_grad()
         X_v = Variable(data, requires_grad=True).to(device)
         output = u_model(X_v)  
+        output_ini = u_model(X_ini)
         output_bdry = u_model(X_bdry)
         
-        u_xx, u_yy = calculate_all_partial(output, X_v)
-        loss1 = loss_f(u_xx + u_yy + (k**2)*output - q(X_v), torch.zeros_like(output))
-        constraint = loss_f(output_bdry, torch.zeros_like(output_bdry))
-        
-        # adam optimizer
+        u_t, u_x, u_xx = calculate_all_partial(output, X_v)
+        loss1 = loss_f(u_t + output*u_x - nu*u_xx, torch.zeros_like(u_t))
+        loss2 = loss_f(output_ini-u_ini, torch.zeros_like(output_ini))
+        loss3 = loss_f(output_bdry, torch.zeros_like(output_bdry))
+
+        # unconstrained Adam
         if dual_opt is None :
-            loss = loss1 + beta*constraint
+            loss = loss1 + beta*loss2 + beta*loss3
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
 
         elif dual_opt is not None:
-            threshold = 0.01
-            constraint = constraint - threshold
+            threshold = 1e-4
+            constraints = torch.stack([loss2, loss3], dim=0)
+            constraints = constraints - threshold
 
             # compute the lagrangian value
-            lagrangian = dual_opt.forward_update(loss1, constraint.unsqueeze(0))
+            lagrangian = dual_opt.forward_update(loss1, constraints)
             lagrangian.backward()
             optimizer.step()
             optimizer.zero_grad()
-
         
         u_model.eval()
         val_err = torch.linalg.norm((u_model(X_val) - y_val),2).item() / torch.linalg.norm(y_val,2).item()
         test_err = torch.linalg.norm((u_model(X_test) - y_test),2).item() / torch.linalg.norm(y_test,2).item()
 
-        loss_list.append((loss1+constraint).item())
+        loss_list.append((loss1+loss2+loss3).item())
         loss_list1.append(loss1.item())
-        loss_list2.append(constraint.item())
+        loss_list2.append(loss2.item())
+        loss_list3.append(loss3.item())
         val_list.append(val_err)
         test_list.append(test_err)
         
-    return np.mean(loss_list), np.mean(loss_list1), np.mean(loss_list2), np.mean(val_list), np.mean(test_list)
+    return np.mean(loss_list), np.mean(loss_list1), np.mean(loss_list2),\
+           np.mean(loss_list3), np.mean(val_list), np.mean(test_list)
 
 
 def main_function(model_name, beta, lr, EPOCH, device) :
     
     # Dataset Creation
+    tmin, tmax = 0, 1
     xmin, xmax = -1,1
-    ymin, ymax = -1,1
-    Nx, Ny = 51, 51
-    X_train = torch.FloatTensor(np.mgrid[xmin:xmax:51j, ymin:ymax:51j].reshape(2, -1).T).to(device)
+    Ns, Nx = 51, 51
+    X_train = torch.FloatTensor(np.mgrid[tmin:tmax:51j, xmin:xmax:51j].reshape(2, -1).T).to(device)
 
+    # Initial Conditions
+    X_ini = X_train[X_train[:,0]==tmin]
+    u_ini = -torch.sin(np.pi*X_ini[:,1].view(-1,1))
+                                
     # Boundary Conditions
-    X_bdry = X_train[(X_train[:,0]==xmin) + (X_train[:,0]==xmax) + (X_train[:,1]==ymin) + (X_train[:,1]==ymax)]
+    X_bdry = X_train[(X_train[:,1]==xmin) + (X_train[:,1]==xmax)]
     u_bdry = torch.zeros_like(X_bdry[:,0]).to(device).view(-1,1)
     
-    X_test, y_test, X_val, y_val= torch.load('./PDEs/Helmholtz/Helmholtz_test', map_location=device)
+    # Validation & Test Set
+    X_test, y_test, X_val, y_val= torch.load('./PDEs/Viscous_Burgers/Burgers_test', map_location=device)
     
     # Make dataloader
     data_train = TensorDataset(X_train)
@@ -112,26 +112,31 @@ def main_function(model_name, beta, lr, EPOCH, device) :
     optimizer=torch.optim.Adam([{'params': u_model.parameters()}], lr=lr)
     best_model = copy.deepcopy(u_model)
 
-    # unconstrained ADAM
     for t in tqdm(range(0, EPOCH)) :
 
-        loss, loss1, loss2, val_err, test_err = train(u_model, beta, trainloader=train_loader,\
-                                                      bdry_data=[X_bdry, u_bdry],\
+        loss, loss1, loss2, loss3, val_err, test_err = train(u_model, beta, trainloader=train_loader,\
+                                                      ini_bdry_data=[X_ini, u_ini, X_bdry, u_bdry],\
                                                       val_test = [X_val, y_val, X_test, y_test],\
-                                                      optimizer=optimizer, loss_f=nn.MSELoss())
+                                                      optimizer=optimizer, loss_f=nn.MSELoss(),
+                                                      dual_opt=None)
         
+        # val_errs.append(torch.linalg.norm((u_model(X_val) - y_val),2).item() / torch.linalg.norm(y_val,2).item())    
+        # test_errs.append(torch.linalg.norm((u_model(X_test) - y_test),2).item() / torch.linalg.norm(y_test,2).item())    
+        # total_loss.append(loss)
+
         val_errs.append(val_err)
         test_errs.append(test_err)
         total_loss.append(loss)
-        constraints.append(loss2)
-        
-        #Print Log
+        constraints.append([loss2, loss3])    # append both costraint
+
+        # Print Log
         if t%100 == 0 :
             print("%s/%s | loss: %06.6f | loss_f: %06.6f | loss_u: %06.6f | val error : %06.6f | test error : %06.6f " % \
-                  (t, EPOCH, loss, loss1, loss2, val_err, test_err))
+                  (t, EPOCH, loss, loss1, loss2+loss3, val_err, test_err))
 
         if np.argmin(val_errs) == t :
             best_model = copy.deepcopy(u_model)
+
 
 
     # SPBM
@@ -140,15 +145,15 @@ def main_function(model_name, beta, lr, EPOCH, device) :
     u_model = set_model(model_name, device)
     
     # Define data and optimizers
-    optimizer = MoreauEnvelope(torch.optim.Adam([{'params': u_model.parameters()}], lr=lr), mu=2.0)
+    optimizer = MoreauEnvelope(torch.optim.Adam([{'params': u_model.parameters()}], lr=0.005), mu=2.0)
     
     dual = PBM(
-        m=1,
+        m=2,
         # penalty_update='dimin',
         # penalty_update='dimin_adapt',
         penalty_update='const',
         pbf = 'quadratic_logarithmic',
-        gamma=0.1,
+        gamma=0.9,
         init_duals=0.01,
         init_penalties=1.,
         penalty_range=(0.5, 1.),
@@ -160,8 +165,8 @@ def main_function(model_name, beta, lr, EPOCH, device) :
 
     for t in tqdm(range(0, EPOCH)) :
 
-        loss, loss1, loss2, val_err, test_err = train(u_model, beta, trainloader=train_loader,\
-                                                      bdry_data=[X_bdry, u_bdry],\
+        loss, loss1, loss2, loss3, val_err, test_err = train(u_model, beta, trainloader=train_loader,\
+                                                      ini_bdry_data=[X_ini, u_ini, X_bdry, u_bdry],\
                                                       val_test = [X_val, y_val, X_test, y_test],\
                                                       optimizer=optimizer, loss_f=nn.MSELoss(),
                                                       dual_opt=dual)
@@ -169,12 +174,12 @@ def main_function(model_name, beta, lr, EPOCH, device) :
         val_errs_spbm.append(val_err)
         test_errs_spbm.append(test_err)
         total_loss_spbm.append(loss)
-        constraints_spbm.append(loss2)
+        constraints_spbm.append([loss2, loss3])
 
         #Print Log
         if t%100 == 0 :
             print("%s/%s | loss: %06.6f | loss_f: %06.6f | loss_u: %06.6f | val error : %06.6f | test error : %06.6f " % \
-                  (t, EPOCH, loss, loss1, loss2, val_err, test_err))
+                  (t, EPOCH, loss, loss1, loss2+loss3, val_err, test_err))
 
         if np.argmin(val_errs_spbm) == t :
             best_model = copy.deepcopy(u_model)
@@ -185,10 +190,10 @@ def main_function(model_name, beta, lr, EPOCH, device) :
     u_model = set_model(model_name, device)
     
     # Define data and optimizers
-    optimizer = MoreauEnvelope(torch.optim.Adam([{'params': u_model.parameters()}], lr=lr), mu=2.0)
+    optimizer = MoreauEnvelope(torch.optim.Adam([{'params': u_model.parameters()}], lr=0.005), mu=2.0)
     
     dual = ALM(
-        m=1,
+        m=2,
         lr=0.1,
         momentum=0.5,
         device=device   
@@ -196,8 +201,8 @@ def main_function(model_name, beta, lr, EPOCH, device) :
 
     for t in tqdm(range(0, EPOCH)) :
 
-        loss, loss1, loss2, val_err, test_err = train(u_model, beta, trainloader=train_loader,\
-                                                      bdry_data=[X_bdry, u_bdry],\
+        loss, loss1, loss2, loss3, val_err, test_err = train(u_model, beta, trainloader=train_loader,\
+                                                      ini_bdry_data=[X_ini, u_ini, X_bdry, u_bdry],\
                                                       val_test = [X_val, y_val, X_test, y_test],\
                                                       optimizer=optimizer, loss_f=nn.MSELoss(),
                                                       dual_opt=dual)
@@ -205,14 +210,14 @@ def main_function(model_name, beta, lr, EPOCH, device) :
         val_errs_alm.append(val_err)
         test_errs_alm.append(test_err)
         total_loss_alm.append(loss)
-        constraints_alm.append(loss2)
-        
+        constraints_alm.append([loss2, loss3])
+
         #Print Log
         if t%100 == 0 :
             print("%s/%s | loss: %06.6f | loss_f: %06.6f | loss_u: %06.6f | val error : %06.6f | test error : %06.6f " % \
-                  (t, EPOCH, loss, loss1, loss2, val_err, test_err))
+                  (t, EPOCH, loss, loss1, loss2+loss3, val_err, test_err))
 
-        if np.argmin(val_errs_spbm) == t :
+        if np.argmin(val_errs_alm) == t :
             best_model = copy.deepcopy(u_model)
 
 
@@ -234,15 +239,19 @@ def main_function(model_name, beta, lr, EPOCH, device) :
     axes[1].set_ylabel('Test Error')
     axes[1].legend()
 
-    axes[2].plot(constraints, label='Adam')
-    axes[2].plot(constraints_spbm, label='SPBM')
-    axes[2].plot(constraints_alm, label='ALM')
+    # plot both constraints + methods shuold have the same color but dashed vs solid
+    axes[2].plot([c[0] for c in constraints], label='Adam - Initial Condition', linestyle='--', color='blue')
+    axes[2].plot([c[1] for c in constraints], label='Adam - Boundary Condition', linestyle='-', color='blue')
+    axes[2].plot([c[0] for c in constraints_spbm], label='SPBM - Initial Condition', linestyle='--', color='green')
+    axes[2].plot([c[1] for c in constraints_spbm], label='SPBM - Boundary Condition', linestyle='-', color='green')   
+    axes[2].plot([c[0] for c in constraints_alm], label='ALM - Initial Condition', linestyle='--', color='red')
+    axes[2].plot([c[1] for c in constraints_alm], label='ALM - Boundary Condition', linestyle='-', color='red')
     axes[2].set_xlabel('Epoch')
-    axes[2].set_ylabel('Boundary Constraint Violation')
+    axes[2].set_ylabel('Constraint Violation')
     axes[2].legend()
 
     plt.tight_layout(pad=2.0)  # extra padding between subplots
-    plt.savefig('./PDEs/Helmholtz/results.png', bbox_inches='tight')
+    plt.savefig('./PDEs/Viscous_Burgers/results.png', bbox_inches='tight')
     
     
 if __name__ == "__main__":
@@ -250,8 +259,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', default='deep_narrow', help='Specify the model. Choose one of [deep_narrow, shallow_wide, deep_narrow_resent, shallow_wide_resnet].')
     parser.add_argument('--beta', default=1, type=float, help='Penalty parameter beta')
-    parser.add_argument('--lr', default=1e-4, type=float, help='Learning rate')
-    parser.add_argument('--EPOCH', default=4000, type=int, help='Number of training EPOCH')
+    parser.add_argument('--lr', default=1e-3, type=float, help='Learning rate')
+    parser.add_argument('--EPOCH', default=1000, type=int, help='Number of training EPOCH')
     parser.add_argument('--ordinal', default=0, type=int, help='Specify the cuda device ordinal.')
     args = parser.parse_args()
     
