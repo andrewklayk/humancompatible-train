1b3e5923f0a28f362fe71d35bdac67b1abe7fc20
bosakad1 <bosakad1@login3.rci.cvut.cz>
2026-06-18 17:06:04 +0200

    gridsearch bug fixes

--- benchmark/scripts/grid_cifar10_pbm.sh
+++ benchmark/scripts/grid_cifar10_pbm.sh
@@ -9,7 +9,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=h200fast
 
-ml PyTorch/2.9.1-foss-2025b-CUDA-12.9.1
+ml PyTorch/2.10.0-foss-2025b-CUDA-12.9.1
 ml Hydra/1.3.2-GCCcore-14.3.0
 ml torchvision/0.25.0-foss-2025b-CUDA-12.9.1
 
