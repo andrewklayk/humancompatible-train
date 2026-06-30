5d7075866f0e1bdc3793752721a37c713714ba5a
bosakad1 <bosakad1@login3.rci.cvut.cz>
2026-06-30 11:50:21 +0200

    E.sh script for e1 and e2

---
+++ benchmark/scripts/new/E1_2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=grid_weight
+#SBATCH --error=./results/logs/grid_weight%a.err
+#SBATCH --output=./results/logs/grid_weight%a.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=5
+#SBATCH --gres=gpu:1
+#SBATCH --time=24:00:00
+#SBATCH --partition=amdgpu
+#SBATCH --array=0-4
+
+# run this (running)
+
+ml PyTorch/2.10.0-foss-2025b-CUDA-12.9.1
+ml Hydra/1.3.2-GCCcore-14.3.0
+ml torchvision/0.25.0-foss-2025b-CUDA-12.9.1
+source ../env_humancompatible/bin/activate
+
+python3 -u run_gridsearch.py task=weight_norm data=income n_epochs=30 task.seed=$SLURM_ARRAY_TASK_ID task.algorithms="[alm_max]"
