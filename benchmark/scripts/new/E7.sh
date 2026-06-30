d5a1132f3224d906cc50f3944c6bda7077edf5e0
andrewklayk <andrew.klyachkin@gmail.com>
2026-06-30 11:38:30 +0200

    sh scripts

--- benchmark/scripts/new/E7.sh
+++ benchmark/scripts/new/E7.sh
@@ -7,9 +7,9 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
 #SBATCH --mem=10G
-#SBATCH --time=4:00:00
-#SBATCH --partition=h200fast
-#SBATCH --array=0-2
+#SBATCH --time=8:00:00
+#SBATCH --partition=h200
+#SBATCH --array=0-4
 
 ml PyTorch/2.10.0-foss-2025b-CUDA-12.9.1
 ml Hydra/1.3.2-GCCcore-14.3.0
